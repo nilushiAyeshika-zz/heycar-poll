7024edbe8376aa130965509db82e9b96022fda61
nilushi_voiceiq <nilushi@voiceiq.ai>
2021-11-22 10:50:23 +0530

    Updated service workers

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 
-import swConfig from './swConfig'
+import swDev from './swDev'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import store from './store'
@@ -22,4 +22,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()
-swConfig()
+swDev()
